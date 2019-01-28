88ac717018f06601462682f421cb94437c536228
IvanBoban <43169071+IvanBoban@users.noreply.github.com>
2019-01-28 21:36:02 +0100

    Add files via upload

---
+++ fibonacci/ConsoleApp2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            Console.WriteLine("Enter untill witch number of the Fibonacci numbers you wish to print:");
+            int.TryParse(Console.ReadLine(), out n);
+            int first = 0;
+            int second = 1;
+            int aux=0;
+
+            for (int i = 0; i < n; i++)
+            {
+                first = second;
+                second = aux;
+                aux = first + second;
+                Console.WriteLine(aux);
+
+
+            }
+            
+
+        }
+    }
+}
